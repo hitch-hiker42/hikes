a8c720c128f96adb390e49d4e7367fc5c5396e7c
Jatin Khandual <39887790+hitch-hiker42@users.noreply.github.com>
2020-02-24 09:15:26 +0530

    Update CHEFK1.cpp

--- Problems/Codechef/CHEFK1.cpp
+++ Problems/Codechef/CHEFK1.cpp
@@ -3,26 +3,26 @@
 using namespace std;
 
 int main() {
-  ios_base::sync_with_stdio(false);
-  cout.tie(nullptr);
-  cin.tie(nullptr);
-  int t = 1; cin >> t;
-	while(t--) {
-	    long long int n, m; cin >> n >> m;
-	    long long int min = n - 1, max = (n * (n + 1)) / 2;
-	    if(m >= min and m <= max) {
-	        long long int offset = m - min, degsum;
+    ios_base::sync_with_stdio(false);
+    cout.tie(nullptr);
+    cin.tie(nullptr);
+    int t = 1; cin >> t;
+    while(t--) {
+        long long int n, m; cin >> n >> m;
+        long long int min = n - 1, max = (n * (n + 1)) / 2;
+        if(m >= min and m <= max) {
+            long long int offset = m - min, degsum;
 	        if(offset <= n) {
-              degsum = 2 * min + offset;
-          } else {
+	            degsum = 2 * min + offset;
+	        } else {
 	            long long int d = 2 * min + n;
 	            degsum = d + 2 * (offset - n);
 	        }
 	        cout << ceil((long double)degsum / n);
-	    } else {
+        } else {
 	        cout << "-1";
-      }
-      cout << '\n';
+        }
+        cout << '\n';
 	}
 	return 0;
 } //farewell.. until we meet again;
