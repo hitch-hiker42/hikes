f2463e4e02028fec6bf507b2cfdedc68c66dd8e9
Jatin Khandual <39887790+hitch-hiker42@users.noreply.github.com>
2020-03-28 07:14:56 +0530

    Create 11044: Searching for Nessy.cpp

---
+++ Sourcestream/UVA/11044: Searching for Nessy.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t; cin >> t;
+    while(t--) {
+        int n, m; cin >> n >> m;
+        cout << (n / 3) * (m / 3) << endl;
+    }
+    return 0;
+}
