e969825283c90df35e27201981f295734d560fd1
Jatin Khandual <39887790+hitch-hiker42@users.noreply.github.com>
2020-03-31 13:13:32 +0530

    Create End Draft.cpp

---
+++ Sourcestream/LeetCode/Binary Search/33. Search in Rotated Sorted Array/End Draft.cpp
@@ -0,0 +1,23 @@
+struct Solution {
+    int search(vector<int>& a, int target) {
+        int n = (int)a.size(), lo = 0, hi = n - 1, m;
+        if(!n) return -1;
+        while(lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            if(a[mid] > a.back()) {
+                lo = mid + 1;
+            } else {
+                m = mid;
+                hi = mid - 1; 
+            }
+        }
+        target > a.back() ? (lo = 0, hi = m - 1) : (lo = m, hi = n - 1);
+        while(lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            if(a[mid] == target) return mid;
+            if(a[mid] < target) lo = mid + 1;
+            else hi = mid - 1;
+        }
+        return - 1;
+    }
+};
