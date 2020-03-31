b395f45f438b8f831c0b186eb76f8f9daed81609
Jatin Khandual <39887790+hitch-hiker42@users.noreply.github.com>
2020-03-31 11:58:57 +0530

    Create elegance.cpp

---
+++ Sourcestream/LeetCode/Binary Search/33. Search in Rotated Sorted Array/elegance.cpp
@@ -0,0 +1,13 @@
+struct Solution {
+    int search(vector<int>& a, int target) {
+        #define span(v) begin(v), end(v)
+        constexpr int infinity = numeric_limits<int>::max();
+        auto skip_left = [&](int x) { return x > a.back() ? -infinity : x; };
+        auto skip_right = [&](int x) { return x <= a.back() ? infinity : x; };
+        auto adjust = [&](int x) { return target <= a.back() ? skip_left(x) : skip_right(x); };
+        auto predicate = [&](int lhs, int rhs) { return adjust(lhs) < adjust(rhs); };
+        auto it = lower_bound(span(a), target, predicate);
+        return it != end(a) && *it == target ? distance(begin(a), it) : -1;
+        #undef span(v)
+    }
+};
