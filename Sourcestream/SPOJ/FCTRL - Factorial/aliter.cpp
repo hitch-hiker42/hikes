74ab9de7643889408cf018d12aed3a6335219ca7
Jatin Khandual <39887790+hitch-hiker42@users.noreply.github.com>
2020-04-15 12:23:49 +0530

    Update aliter.cpp

--- Sourcestream/SPOJ/FCTRL - Factorial/aliter.cpp
+++ Sourcestream/SPOJ/FCTRL - Factorial/aliter.cpp
@@ -3,18 +3,18 @@
 using namespace std;
 
 int64_t itoa(int64_t n, int64_t k = 10) {
-	if(!n) return 0;
-	return (n % k) + 10 * itoa(n / k, k);
+    if(!n) return 0;
+    return (n % k) + 10 * itoa(n / k, k);
 }
 
 void hike() {
-	int64_t n, x, sum = 0; cin >> n, x = itoa(n, 5);
-	while(x) sum += x % 10, x /= 10;
-	cout << (n - sum) / 4 << endl;
+    int64_t n, x, sum = 0; cin >> n, x = itoa(n, 5);
+    while(x) sum += x % 10, x /= 10;
+    cout << (n - sum) / 4 << endl;
 }
 
 signed main() {
-	int t; cin >> t;
-	while(t--) hike();
-	return 0;
+    int t; cin >> t;
+    while(t--) hike();
+    return 0;
 } //farewell, until we meet again;
