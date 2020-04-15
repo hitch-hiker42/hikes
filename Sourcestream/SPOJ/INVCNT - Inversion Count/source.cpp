e5622fffe978b317c3d0fb713eef588fdc324f49
Jatin Khandual <39887790+hitch-hiker42@users.noreply.github.com>
2020-04-15 12:28:01 +0530

    Update source.cpp

--- Sourcestream/SPOJ/INVCNT - Inversion Count/source.cpp
+++ Sourcestream/SPOJ/INVCNT - Inversion Count/source.cpp
@@ -1,3 +1,4 @@
+//author: hitch_hiker42;
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -8,47 +9,46 @@ using namespace std;
 constexpr int sentinel = numeric_limits<int>::max();
 
 int merge(auto& a, int lo, int mid, int hi) {
-	int p = mid - lo + 1, q = hi - mid, inv = 0;
-	vector<int> left, right;
-	copy_n(begin(a) + lo, p, back_inserter(left));
-	copy_n(begin(a) + mid + 1, q, back_inserter(right));
-	left.emplace_back(sentinel); right.emplace_back(sentinel);
-	register int i = 0, j = 0, k = lo;
-	while(k <= hi) {
-		if(left[i] <= right[j]) {
-			a[k++] = left[i++];
-		} else {
-			inv += p - i;
-			a[k++] = right[j++];
-		}
+    int p = mid - lo + 1, q = hi - mid, inv = 0;
+    vector<int> left, right;
+    copy_n(begin(a) + lo, p, back_inserter(left));
+    copy_n(begin(a) + mid + 1, q, back_inserter(right));
+    left.emplace_back(sentinel); 
+    right.emplace_back(sentinel);
+    register int i = 0, j = 0, k = lo;
+    while(k <= hi) {
+	if(left[i] <= right[j]) {
+	    a[k++] = left[i++];
+	} else {
+	    inv += p - i;
+	    a[k++] = right[j++];
 	}
-	return inv;
+    }
+    return inv;
 }
 
 int mergesort(auto& a, int lo, int hi) {
-	int inv{0};
-	if(lo < hi) {
-		int mid  = lo + ((hi - lo) >> 1);
-		inv += mergesort(a, lo, mid) + mergesort(a, mid + 1, hi) + merge(a, lo, mid, hi);
-	}
-	return inv;
+    int inv{0};
+    if(lo < hi) {
+        int mid  = lo + ((hi - lo) >> 1);
+	inv += mergesort(a, lo, mid) + mergesort(a, mid + 1, hi) + merge(a, lo, mid, hi);
+    }
+    return inv;
 }
 
-void solve() {
-	int n; cin >> n;
-	vector<int> a(n);
-	for_each(span(a), [&](int& i) {
-		cin >> i;
-	});
-	cout << mergesort(a, 0, n - 1) << endl;	
+void hike() {
+    int n; cin >> n;
+    vector<int> a(n);
+    for_each(span(a), [&](int& i) {
+        cin >> i;
+    });
+    cout << mergesort(a, 0, n - 1) << endl;	
 }
 
 signed main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr); cout.tie(nullptr);
-	int t; cin >> t;
-	while(t--) {
-		solve();
-	}
-	return 0;
-}
+    int t; cin >> t;
+    while(t--) hike();
+    return 0;
+} //farewell, until we meet again..
