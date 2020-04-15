b51434718808a6c52e660d6aabd5520f508cc787
Jatin Khandual <39887790+hitch-hiker42@users.noreply.github.com>
2020-04-15 12:29:28 +0530

    Update source.cpp

--- Sourcestream/SPOJ/PTO7Z - Longest Path In A Tree/source.cpp
+++ Sourcestream/SPOJ/PTO7Z - Longest Path In A Tree/source.cpp
@@ -3,28 +3,28 @@
 using namespace std;
 
 pair<int, int> dfs(auto& adj, int u, int p, int lvl) {
-	pair<int, int> global = {u, lvl};
-	for(auto& v: adj[u]) if(v != p) {
-		auto local = dfs(adj, v, u, lvl + 1);
-		if(local.second > global.second) global = local;
-	}
-	return global;
+    pair<int, int> global = {u, lvl};
+    for(auto& v: adj[u]) if(v != p) {
+        auto local = dfs(adj, v, u, lvl + 1);
+	if(local.second > global.second) global = local;
+    }
+    return global;
 }
 
 void hike() {
-	int n; cin >> n;
-	vector<vector<int>> adj(n);
-	for(int i = 1, u, v; i < n; ++i) {
-		cin >> u >> v;
-		adj[u - 1].emplace_back(v - 1);
-		adj[v - 1].emplace_back(u - 1);
-	}
-	auto x = dfs(adj, 0, -1, 0), y = dfs(adj, x.first, -1, 0);
-	cout << y.second << endl;
+    int n; cin >> n;
+    vector<vector<int>> adj(n);
+    for(int i = 1, u, v; i < n; ++i) {
+        cin >> u >> v;
+	adj[u - 1].emplace_back(v - 1);
+	adj[v - 1].emplace_back(u - 1);
+    }
+    auto x = dfs(adj, 0, -1, 0), y = dfs(adj, x.first, -1, 0);
+    cout << y.second << endl;
 }
 
 signed main() {
-	int t = 1; //cin >> t;
-	while(t--) hike();
-	return 0;
+    int t = 1; //cin >> t;
+    while(t--) hike();
+    return 0;
 } //farewell, until we meet again..
