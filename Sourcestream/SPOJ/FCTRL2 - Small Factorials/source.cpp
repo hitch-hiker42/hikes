35ce3960d7d7b2ae77c4888509e0c6dc2e4b6a03
Jatin Khandual <39887790+hitch-hiker42@users.noreply.github.com>
2020-04-16 16:17:48 +0530

    Update source.cpp

--- Sourcestream/SPOJ/FCTRL2 - Small Factorials/source.cpp
+++ Sourcestream/SPOJ/FCTRL2 - Small Factorials/source.cpp
@@ -19,7 +19,7 @@ void hike() {
     for(int i = 2; i <= n; ++i) {
         for(int j = 0, x, y, z, carry = 0; j < d; ++j) {
 	    x = factorial[j] - '0';
-	    y = x * i + carry;
+            y = x * i + carry;
 	    z = y % 10;
 	    carry = y / 10;
 	    factorial[j] = z + '0';
