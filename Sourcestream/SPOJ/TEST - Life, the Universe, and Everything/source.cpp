5cff193e6628a1201f18138ac6ec2c67639d6fe1
Jatin Khandual <39887790+hitch-hiker42@users.noreply.github.com>
2020-04-16 18:49:10 +0530

    Create source.cpp

---
+++ Sourcestream/SPOJ/TEST - Life, the Universe, and Everything/source.cpp
@@ -0,0 +1,8 @@
+//author: hitch_hiker42;
+#include<bits/stdc++.h>
+using namespace std;
+
+signed main() {
+    for(short x; cin >> x, x != 42; cout << x << endl);
+    return 0;
+} //farewell, until we meet again..
