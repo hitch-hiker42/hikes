b72db42c692322145ba42a119cf5fc57a8f39e7f
Jatin Khandual <39887790+hitch-hiker42@users.noreply.github.com>
2020-04-18 14:42:32 +0530

    Update source.cpp

--- Dynamic Programming/Counting Problems/Counting sub-trees in a tree/source.cpp
+++ Dynamic Programming/Counting Problems/Counting sub-trees in a tree/source.cpp
@@ -1,19 +1,19 @@
+//author: hitch_hiker42;
 #include<bits/stdc++.h>
 using namespace std;
 
 constexpr int lim = 100001;
-map<int, vector<int>> g;
 int64_t dp[lim];
 bool vis[lim];
 
 //returns the number of subtrees rooted at vertex u:
-int64_t dfs(int u) {
+int64_t dfs(auto& adj, int u) {
     int64_t& state = dp[u];
     vis[u] = true;
     if(~state) return state;
     state = 1;
-    for(int& v: g[u]) if(!vis[v]) {
-        state = state * (dfs(v) + 1);
+    for(int& v: adj[u]) if(!vis[v]) {
+        state = state * (dfs(adj, v) + 1);
     }
     return state;
 }
@@ -22,15 +22,16 @@ int main() {
     int n, s; cin >> n;
     memset(dp, -1, sizeof dp);
     memset(vis, false, sizeof vis);
+    vector<vector<int>> adj(n + 1);
     for(int i = 0; i < n - 1; ++i) {
         int u, v; cin >> u >> v;
-        g[u].emplace_back(v);
-        g[v].emplace_back(u);
+        adj[u].emplace_back(v);
+        adj[v].emplace_back(u);
     }
     int x, k{0}; cin >> s;
-    x = dfs(s);
+    x = dfs(adj, s);
     //for(int i = 1; i <= n; ++i) cout << dp[i] << ' ';
     cout << "Number of subtrees rooted at node " << s << ": " << x << endl;
     cout << "Total number of subtrees of the given tree: " << k << endl;
     return 0;
-}
+} //farewell, until we meet again..
