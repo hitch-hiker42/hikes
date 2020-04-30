51e5700b35cd1594b79922b605058eb21e3b7587
Jatin Khandual <39887790+hitch-hiker42@users.noreply.github.com>
2020-04-30 20:55:13 +0530

    Create source.cpp

---
+++ Sourcestream/Hackerearth/Find Me!/source.cpp
@@ -0,0 +1,8 @@
+//author: hitch_hiker42;
+#include<bits/stdc++.h>
+using namespace std;
+
+signed main() {
+    int n, a, b; cin >> n >> a >> b;
+    cout << n - (a + b) << endl;
+} //farewell, until we meet again..
