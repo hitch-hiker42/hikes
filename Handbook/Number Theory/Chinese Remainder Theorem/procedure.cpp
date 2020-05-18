7ed751260271c9d7dd550bb652353a0f878c4277
Jatin Khandual <39887790+hitch-hiker42@users.noreply.github.com>
2020-05-19 01:29:47 +0530

    Update procedure.cpp

--- Handbook/Number Theory/Chinese Remainder Theorem/procedure.cpp
+++ Handbook/Number Theory/Chinese Remainder Theorem/procedure.cpp
@@ -1,7 +1,7 @@
 //author: hitch_hiker42;
 auto gcd(int a, int b) {
     if(!b) return make_tuple(a, (int)1, (int)0);
-    auto [d, x, y] = gcd(a, b);
+    auto [d, x, y] = gcd(b, a % b);
     return make_tuple(d, y, x - (a / b) * y);
 }
 
