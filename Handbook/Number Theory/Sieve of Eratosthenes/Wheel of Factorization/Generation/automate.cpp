1105745c05c1d32cd4604b4c9934a5bc427d92c9
Jatin Khandual <39887790+hitch-hiker42@users.noreply.github.com>
2020-05-29 10:51:21 +0530

    Update automate.cpp

--- Handbook/Number Theory/Sieve of Eratosthenes/Wheel of Factorization/Generation/automate.cpp
+++ Handbook/Number Theory/Sieve of Eratosthenes/Wheel of Factorization/Generation/automate.cpp
@@ -1,13 +1,23 @@
-//skiplists:
-cout << "{";
-for(int i = 1; i < cycle[3].size(); i++) {
-    cout << cycle[3][i] - cycle[3][i - 1] << ", ";
-}
-cout << "}";
+//basis:
+length: b, composed of the first 'b' natural primes
 
-//hashes:
-cout << "{";
-for(int i = 0; i < cycle[3].size() - 1; i++) {
-    cout << "{" << cycle[3][i]<< ", " << i << "}, ";
+//circumference:
+w[b - 1] = product of elements of the basis
+
+//stokes:
+vector<bool> p(w[b - 1], true);
+p[0] = false;
+for(int& i: basis) {
+    for(int j = i * i; j < w[b - 1]; j += i) p[j] = false;
 }
-cout << "}";
+for(int i = 1; i < w[b - 1]; ++i) if(p[i]) stokes[b - 1].emplace_back(i);
+
+//skiplists:
+for(int i = 1; i < (int)stokes[b - 1].size(); i++) {
+    skip[b - 1].emplace_back(stokes[b - 1][i] - stokes[b - 1][i - 1]);
+}
+
+//wheel hashes:
+for(int i = 0; i < (int)stokes[b - 1].size() - 1; i++) {
+    wheel[stokes[b - 1][i]] = i;
+}
