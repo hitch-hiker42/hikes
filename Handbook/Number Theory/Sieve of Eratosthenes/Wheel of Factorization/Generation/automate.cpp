62c71358ac99f4767f11eed10b1369a8638c378c
Jatin Khandual <39887790+hitch-hiker42@users.noreply.github.com>
2020-05-29 11:13:21 +0530

    Update automate.cpp

--- Handbook/Number Theory/Sieve of Eratosthenes/Wheel of Factorization/Generation/automate.cpp
+++ Handbook/Number Theory/Sieve of Eratosthenes/Wheel of Factorization/Generation/automate.cpp
@@ -8,18 +8,18 @@ w[b - 1] = product of elements of the basis
 vector<bool> p(w[b - 1], true);
 p[0] = false;
 for(int& i: basis) {
-	p[i] = false;
+    p[i] = false;
     for(int j = i * i; j < w[b - 1]; j += i) p[j] = false;
 }
 for(int i = 1; i < w[b - 1]; ++i) if(p[i]) stokes[b - 1].emplace_back(i);
 
 //skiplists:
-for(int i = 1; i < (int)stokes[b - 1].size(); ++i) {
+for(int i = 1, z = (int)stokes[b - 1].size(); i < z; ++i) {
     skip[b - 1].emplace_back(stokes[b - 1][i] - stokes[b - 1][i - 1]);
 }
 skip[b - 1].emplace_back(2);
 
 //wheel hashes:
-for(int i = 0; i < (int)stokes[b - 1].size() - 1; i++) {
-    wheel[stokes[b - 1][i]] = i;
+for(int i = 0, z = (int)stokes[b - 1].size(); i < z; ++i) {
+    wheel[b - 1][stokes[b - 1][i]] = i;
 }
