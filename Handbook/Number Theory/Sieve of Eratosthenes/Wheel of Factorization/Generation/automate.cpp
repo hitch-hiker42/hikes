7fe13abf59564cb708e0a4141f258c531664e85f
Jatin Khandual <39887790+hitch-hiker42@users.noreply.github.com>
2020-05-29 11:48:52 +0530

    Update automate.cpp

--- Handbook/Number Theory/Sieve of Eratosthenes/Wheel of Factorization/Generation/automate.cpp
+++ Handbook/Number Theory/Sieve of Eratosthenes/Wheel of Factorization/Generation/automate.cpp
@@ -5,29 +5,29 @@ vector<int> stokes[b], skip[b];
 map<int, int> wheel[b];
 
 void construct() {
-	for(int j = 1; j <= b; ++j) {
-		vector<int> base(begin(basis), begin(basis) + j);
-		//circumference:
-		w[j - 1] = accumulate(span(base), 1, multiplies<int>());
-		
-		//stokes:
-		vector<bool> p(w[j - 1], true);
-		p[0] = false;
-		for(int& i: base) {
-		    p[i] = false;
-		    for(int k = i * i; k < w[j - 1]; k += i) p[k] = false;
-		}
-		for(int i = 1; i < w[j - 1]; ++i) if(p[i]) stokes[j - 1].emplace_back(i);
+    for(int j = 1; j <= b; ++j) {
+        vector<int> base(begin(basis), begin(basis) + j);
+        //circumference:
+        w[j - 1] = accumulate(span(base), 1, multiplies<int>());
 
-		//skiplists:
-		for(int i = 1, z = (int)stokes[j - 1].size(); i < z; ++i) {
-		    skip[j - 1].emplace_back(stokes[j - 1][i] - stokes[j - 1][i - 1]);
-		}
-		skip[j - 1].emplace_back(2);
+        //stokes:
+        vector<bool> p(w[j - 1], true);
+        p[0] = false;
+        for(int& i: base) {
+            p[i] = false;
+            for(int k = i * i; k < w[j - 1]; k += i) p[k] = false;
+        }
+        for(int i = 1; i < w[j - 1]; ++i) if(p[i]) stokes[j - 1].emplace_back(i);
 
-		//wheel hashes:
-		for(int i = 0, z = (int)skip[j - 1].size(); i < z; ++i) {
-		    wheel[j - 1][stokes[j - 1][i]] = i;
-		}
-	}
+        //skiplists:
+        for(int i = 1, z = (int)stokes[j - 1].size(); i < z; ++i) {
+            skip[j - 1].emplace_back(stokes[j - 1][i] - stokes[j - 1][i - 1]);
+        }
+        skip[j - 1].emplace_back(2);
+
+        //wheel hashes:
+        for(int i = 0, z = (int)skip[j - 1].size(); i < z; ++i) {
+            wheel[j - 1][stokes[j - 1][i]] = i;
+        }
+    }
 } //farewell, until we meet again..
