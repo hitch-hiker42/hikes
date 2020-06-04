e2766ea9758dbbb858bf2dc9e0feaee0b4dc75f6
Jatin Khandual <39887790+hitch-hiker42@users.noreply.github.com>
2020-06-04 11:52:58 +0530

    Update procedure.cpp

--- Handbook/Data Structures/Fenwick Tree/Vanilla/procedure.cpp
+++ Handbook/Data Structures/Fenwick Tree/Vanilla/procedure.cpp
@@ -55,9 +55,9 @@ struct fenwick {
         int run = 0, idx = 0;
         for(int i = log2(n); ~i; --i) {
             if(idx + (1 << i) < n and run + bit[idx + (1 << i)] < p) {
-        		run += bit[idx + (1 << i)];
-        		idx += (1 << i);
-        	}
+                run += bit[idx + (1 << i)];
+                idx += (1 << i);
+            }
         }
         return idx + 1;
     }
