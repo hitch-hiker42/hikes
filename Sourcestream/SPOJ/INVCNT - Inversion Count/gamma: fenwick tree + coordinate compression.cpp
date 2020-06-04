acf383f3bb3baa2fde139666452f07a68e88fbd3
Jatin Khandual <39887790+hitch-hiker42@users.noreply.github.com>
2020-06-04 13:26:42 +0530

    Create gamma: fenwick tree + coordinate compression.cpp

---
+++ Sourcestream/SPOJ/INVCNT - Inversion Count/gamma: fenwick tree + coordinate compression.cpp
@@ -0,0 +1,58 @@
+//author: hitch_hiker42;
+#include "bits/stdc++.h"
+using namespace std;
+
+//solution:
+#define span(a) begin(a), end(a)
+#define int int64_t
+
+struct fenwick {
+    int n, inv = 0;
+    vector<int> bit;
+    fenwick(int n) {
+        this -> n = n + 1;
+        bit.assign(n + 1, 0);
+    }
+    fenwick(vector<int>& a): fenwick(*max_element(span(a))) {
+        for(int i = (int)a.size() - 1; ~i; --i) {
+            inv += sum(a[i] - 1);
+            update(a[i], 1);
+        }
+    }
+    int sum(int r) {
+        int result = 0;
+        for(int x = r; x; x -= x & -x) result += bit[x];
+        return result;
+    }
+    int sum(int lo, int hi) {
+        return sum(hi) - sum(lo - 1);
+    }
+    void update(int i, int delta) {
+        for(int x = i; x <= n; x += x & -x) bit[x] += delta;
+    }
+};
+
+auto compress(vector<int>& sparse) {
+    int n = sparse.size();
+    vector<int> order(span(sparse)), dense(n);
+    sort(span(order));
+    for(int i = 0; i < n; ++i) {
+        dense[i] = distance(begin(order), lower_bound(span(order), sparse[i])) + 1;
+    }
+    return move(dense);
+}
+
+void hike() {
+    int n; cin >> n;
+    vector<int> a(n);
+    for(int& i: a) cin >> i;
+    a = compress(a);
+    fenwick tree(a);
+    cout << tree.inv << "\n";
+}
+
+signed main() {
+    int t; cin >> t;
+    while(t--) hike();
+    return 0;
+} //farewell, until we meet again..
