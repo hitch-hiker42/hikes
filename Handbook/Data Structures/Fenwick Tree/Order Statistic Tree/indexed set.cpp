3b79f402cdfab29e5ad93a5647b677641ae49cfb
Jatin Khandual <39887790+hitch-hiker42@users.noreply.github.com>
2020-06-06 09:54:56 +0530

    Create indexed set.cpp

---
+++ Handbook/Data Structures/Fenwick Tree/Order Statistic Tree/indexed set.cpp
@@ -0,0 +1,54 @@
+//author: hitch_hiker42;
+struct fenwick {
+    int n;
+    vector<int> bit;
+    fenwick(int n) {
+        this -> n = n;
+        bit.assign(n + 1, 0);
+    }
+    int sum(int i) {
+        int result = 0;
+        while(i) result += bit[i], i -= i & -i;
+        return result;
+    }
+    int sum(int lo, int hi) {
+        return sum(hi) - sum(lo - 1);
+    }
+    void update(int i, int delta) {
+        while(i <= n) bit[i] += delta, i += i & -i;
+    }
+    int read(int i) {
+        int value = bit[i];
+        if(i) {
+            int idx = i - (i & -i);
+            i = i - 1;
+            while(i != idx) {
+                value -= bit[i];
+                i -= i & -i;
+            }
+        }
+        return value;
+    }
+    void insert(int i) {
+        if(read(i) == 1) return;
+        update(i, 1);
+    }
+    void erase(int i) {
+        if(read(i) == 0) return;
+        update(i, -1);
+    }
+    int rank(int i) {
+        return sum(i);
+    }
+    int order_statistic(int k) {
+        if(k > sum(n)) return -1;
+        int run = 0, idx = 0;
+        for(int i = log2(n); ~i; --i) {
+            if(idx + (1 << i) < n and run + bit[idx + (1 << i)] < k) {
+                run += bit[idx + (1 << i)];
+                idx += 1 << i;
+            }
+        }
+        return idx + 1;
+    }
+}; //farewell, until we meet again..
