8b41b484b81ca4c88ded5f90585c2beffb9205eb
Jatin Khandual <39887790+hitch-hiker42@users.noreply.github.com>
2020-06-06 10:08:47 +0530

    Update alpha.cpp

--- Handbook/Data Structures/Coordinate Compression/alpha.cpp
+++ Handbook/Data Structures/Coordinate Compression/alpha.cpp
@@ -1,16 +1,11 @@
 //author: hitch_hiker42;
-void compress(vector<query>& a, int& m, map<int, int>& hash) {
-    int n = a.size();
-    vector<int> u, v;
-    for(int i = 0; i < n; ++i) if(a[i].type != 'K') u.emplace_back(a[i].x);
-    v = u; sort(span(v));
-    m = numeric_limits<int>::min();
-    for(int i = 0, j = 0; i < n; ++i) {
-        if(a[i].type != 'K') {
-            a[i].x = distance(begin(v), lower_bound(span(v), u[j])) + 1;
-            hash[a[i].x] = u[j];
-            m = max(m, a[i].x);
-            j = j + 1;
-        }
+void compress(vector<int>& sparse, int& m, map<int, int>& hash) {
+    int n = sparse.size();
+    vector<int> order(span(sparse)), dense;
+    sort(span(order));
+    for(int i = 0; i < n; ++i) {
+        dense[i] = distance(begin(order), lower_bound(span(order), sparse[i])) + 1;
+        hash[dense[i]] = sparse[i];
     }
+    m = *max_element(span(dense));
 } //farewell, until we meet again..
