0feff48d78d6e202554b841dc363f73d61db718d
Jatin Khandual <39887790+hitch-hiker42@users.noreply.github.com>
2020-06-06 13:23:20 +0530

    Create source.cpp

---
+++ Handbook/Data Structures/Fenwick Tree/Prestige/source.cpp
@@ -0,0 +1,38 @@
+//author: hitch_hiker42;
+struct fenwick {
+    int n;
+    vector<int> bit;
+    fenwick(int n) {
+        this -> n = n;
+        bit.assign(n + 1, 0);
+    }
+    fenwick(vector<int>& a): fenwick(a.size()) {
+        for(int i = 1; i <= n; ++i) update(i, a[i - 1]);
+    }
+    int sum(int i) {
+        int result = 0;
+        for(int x = i; x; x -= x & -x) result += bit[x];
+        return result;
+    }
+    int sum(int lo, int hi) {
+        return sum(hi) - sum(lo - 1);
+    }
+    void update(int i, int delta) {
+        for(int x = i; x <= n; x += x & -x) bit[x] += delta;
+    }
+    int read(int i) {
+        int value = bit[i];
+        if(i) {
+            int idx = i - (i & -i);
+            i = i - 1;
+            while(i != idx) {
+                value -= bit[i];
+                i -= i & -i;
+            }
+        }
+        return value;
+    }
+    void write(int i, int value) {
+        update(i, value - read(i));
+    }
+}; //farewell, until we meet again..
