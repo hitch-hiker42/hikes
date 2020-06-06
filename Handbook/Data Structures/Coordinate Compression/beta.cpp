efa04088563f145339444f98ef26a49b2c59352d
Jatin Khandual <39887790+hitch-hiker42@users.noreply.github.com>
2020-06-06 10:11:53 +0530

    Update beta.cpp

--- Handbook/Data Structures/Coordinate Compression/beta.cpp
+++ Handbook/Data Structures/Coordinate Compression/beta.cpp
@@ -2,9 +2,8 @@
 void compress(vector<int>& a, int& m, map<int, int>& hash) {
     set<int> filter;
     for(auto& i: a) filter.insert(i);
-    int j = 1;
-    for(auto& i: filter) hash[i] = j++;
-    m = j - 1;
+    m = 0;
+    for(auto& i: filter) hash[i] = ++m;
     for(auto& i: a) i = hash[i];
     map<int, int> invert;
     for(auto& [x, y]: hash) invert[y] = x;
