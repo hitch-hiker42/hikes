f4e2796d275fd60de960c99d4c9376fd21a29411
Jatin Khandual <39887790+hitch-hiker42@users.noreply.github.com>
2020-06-17 22:43:29 +0530

    Create source.cpp

---
+++ Sourcestream/Codechef/JUNE20/EOEO - The Tom and Jerry Game/source.cpp
@@ -0,0 +1,15 @@
+//author: hitch_hiker42;
+#include<bits/stdc++.h>
+using namespace std;
+
+//solution:
+void hike() {
+    int64_t n; cin >> n;
+    cout << n / ((n & -n) << 1) << "\n";
+}
+
+signed main() {
+    int t; cin >> t;
+    while(t--) purgatorio();
+    return 0;
+} //farewell, until we meet again!
