0b7c70504e4ee17f07e276b5c90668cb565fcc7c
Jatin Khandual <39887790+hitch-hiker42@users.noreply.github.com>
2020-06-25 04:01:31 +0530

    Update source.cpp

--- Handbook/Data Structures/Monotone Stack/Previous Lesser Element/source.cpp
+++ Handbook/Data Structures/Monotone Stack/Previous Lesser Element/source.cpp
@@ -1,8 +1,8 @@
 //author: hitch_hiker42;
 auto ple(vector<int>& a) {
     int n = a.size();
-    vector<int> premin(n);
     stack<int> s;
+    vector<int> premin(n);
     for(int i = 0; i < n; ++i) {
         while(!s.empty() and a[s.top() - 1] > a[i]) s.pop();
         premin[i] = s.empty() ? -1 : s.top();
