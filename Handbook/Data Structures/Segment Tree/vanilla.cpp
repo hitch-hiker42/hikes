6679e87ee26a286d2b4b4824bfe00cf51dde9f2b
Jatin Khandual <39887790+hitch-hiker42@users.noreply.github.com>
2020-07-14 16:09:47 +0530

    Update vanilla.cpp
    templatized

--- Handbook/Data Structures/Segment Tree/vanilla.cpp
+++ Handbook/Data Structures/Segment Tree/vanilla.cpp
@@ -1,27 +1,25 @@
 //author: hitch_hiker42;
-#define parent(i) (i >> 1)
-#define left(i) (i << 1)
-#define right(i) (left(i) | 1)
-
+template<class T>
 struct segtree {
-    int n, identity;
-    vector<int> t;
-    function<int(int, int)> op;
-    segtree(vector<int> a, function<int(int, int)> bin, int id) {
+    int n;
+    T identity;
+    vector<T> t;
+    function<T(T, T)> op;
+    segtree(vector<int> a, function<T(T, T)> bin, T id) {
         this -> n = a.size();
         this -> op = bin;
         identity = id;
         t.assign(n << 1, identity);
-        for(int i = 0; i < n; ++i) t[n + i] = a[i];
+        for(int i = 0; i < n; ++i) t[n + i] = {a[i], 1};
         for(int i = n - 1; i; --i) {
             t[i] = op(t[left(i)], t[right(i)]);
         }
     }
-    void update(int i, int delta) {
+    void update(int i, T delta) {
         for(t[i += n] = delta; i > 1; i >>= 1) t[parent(i)] = op(t[i], t[i ^ 1]);
     }
-    int query(int lo, int hi) {
-        int result = identity;
+    auto query(int lo, int hi) {
+        T result = identity;
         for(lo += n, hi += n; lo < hi; lo >>= 1, hi >>= 1) {
             if (lo & 1) result = op(result, t[lo++]);
             if (hi & 1) result = op(result, t[--hi]);
