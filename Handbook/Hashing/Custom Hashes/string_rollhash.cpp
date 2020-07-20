d93bfe8c4583a8f68c5c9614e50b5c43a8f88351
Jatin Khandual <39887790+hitch-hiker42@users.noreply.github.com>
2020-07-21 00:36:42 +0530

    Create string_rollhash.cpp

---
+++ Handbook/Hashing/Custom Hashes/string_rollhash.cpp
@@ -0,0 +1,24 @@
+//author: hitch_hiker42;
+#define int int64_t
+
+int rollhash(string const& s) {
+    int hash = 0, x = 1;
+    for(const char& c: s) {
+        (hash += (c - 'a' + 1) * x % mod) %= mod;
+        x = x * p % mod;
+    }
+    return hash;
+}
+
+bool equal(const string& s, const string& t) {
+    return !(s.compare(t));
+}
+
+struct custom {
+    size_t operator () (const string& s) const {
+        return rollhash(s);
+    }
+};
+
+unordered_map<string, int, custom, equal> f;
+// farewell, until we meet again..
