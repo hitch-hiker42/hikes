85e5100db0c7e25a9ab85c4c097645f0210ec82f
Jatin Khandual <39887790+hitch-hiker42@users.noreply.github.com>
2020-08-19 23:32:50 +0530

    Update classic.cpp

--- Sourcestream/SPOJ/FCTRL - Factorial/classic.cpp
+++ Sourcestream/SPOJ/FCTRL - Factorial/classic.cpp
@@ -3,13 +3,13 @@
 using namespace std;
 
 void hike() {
-	int n, p = 5, sum = 0; cin >> n;
-	while(n) n /= p, sum += n;
-	cout << sum << endl;
+    int n, p = 5, sum = 0; cin >> n;
+    while(n) n /= p, sum += n;
+    cout << sum << endl;
 }
 
 signed main() {
-	int t; cin >> t;
-	while(t--) hike();
-	return 0;
+    int t; cin >> t;
+    while(t--) hike();
+    return 0;
 } //farewell, until we meet again..
