d866b0023b1ad154e9d4abd51df175264105a233
Jatin Khandual <39887790+hitch-hiker42@users.noreply.github.com>
2021-04-25 17:48:29 +0530

    Update and rename aliter alpha.cpp to fast.cpp

---
+++ Handbook/Data Structures/Segment Tree/fast.cpp
@@ -0,0 +1,28 @@
+constexpr int mxn = 1e5;
+int n, a[mxn], t[mxn << 1];
+
+int f(int i, int j) {
+  return max(i, j);
+}
+
+void init() {
+  for(int i = 0; i < n; ++i) t[n + i] = a[i];
+  for(int i = n - 1; i; --i) {
+    t[i] = f(t[i << 1], t[(i << 1) | 1]);
+  }
+}
+ 
+void update(int i, int delta) {
+  for(t[i += n] = delta; i > 1; i >>= 1) {
+    t[(i >> 1)] = max(t[i], t[i ^ 1]);
+  }
+}
+ 
+int query(int lo, int hi) {
+  int result = 0;
+  for(lo += n, hi += n; lo < hi; lo >>= 1, hi >>= 1) {
+    if (lo & 1) result = max(result, t[lo++]);
+    if (hi & 1) result = max(result, t[--hi]);
+  }
+  return result;
+}
